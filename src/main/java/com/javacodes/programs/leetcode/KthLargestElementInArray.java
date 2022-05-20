a8c9f2ba76a98db86adf9137064f5a19f3f82f03
monika-sivamoorthy <monikasivamoorthy@gmail.com>
2022-05-20 10:19:48 -0500

    leetcode #215

---
+++ src/main/java/com/javacodes/programs/leetcode/KthLargestElementInArray.java
@@ -0,0 +1,11 @@
+package com.javacodes.programs.leetcode;
+
+import java.util.Arrays;
+
+public class KthLargestElementInArray {
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+        return nums[nums.length-k];
+    }
+}
