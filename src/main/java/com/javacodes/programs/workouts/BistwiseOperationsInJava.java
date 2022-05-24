360041a7db66017a9a600effb047612046fb3637
monika-sivamoorthy <monikasivamoorthy@gmail.com>
2022-05-23 20:58:11 -0500

    Notes

---
+++ src/main/java/com/javacodes/programs/workouts/BistwiseOperationsInJava.java
@@ -0,0 +1,8 @@
+package com.javacodes.programs.workouts;
+
+public class BistwiseOperationsInJava {
+    public static void main(String[] args) {
+        //  math
+        // collections
+    }
+}
---
+++ src/main/java/com/javacodes/programs/workouts/MiscellaneousNotes.java
@@ -0,0 +1,25 @@
+package com.javacodes.programs.workouts;
+
+// List of all Java Keywords: https://www.geeksforgeeks.org/list-of-all-java-keywords/?ref=lbp
+// Enum java - .ordinal, .values(), value() https://www.geeksforgeeks.org/enum-in-java/?ref=lbp
+
+
+// Bitwise operators in jav : https://www.geeksforgeeks.org/bitwise-operators-in-java/?ref=lbp:
+// Shift operators in java
+
+// instance operator object instance of class/subclass/interface
+
+// java pairs : https://www.geeksforgeeks.org/pair-class-in-java/
+// https://www.geeksforgeeks.org/sort-an-array-of-pairs-using-java-pair-and-comparator/?ref=lbp
+
+// collections https://www.geeksforgeeks.org/collections-in-java-2/?ref=lbp
+
+// Character methods:
+// Character.isDigit('1')
+public class MiscellaneousNotes {
+    public static void main(String[] args) {
+
+    }
+}
+
+
